3addb478a0158053e68abf57562408fadcb58772
Paolo Stieger <paolo.stieger@gmail.com>
2016-12-16 16:19:07 +0100

    Added FileHandler.java

---
+++ TTOD2D/src/source/FileHandler.java
@@ -0,0 +1,20 @@
+package source;
+
+/**
+ * Created by pstieger on 16.12.2016.
+ */
+public class FileHandler {
+    private static final String FILE_NAME = "";
+
+    public FileHandler(){
+
+    }
+
+    public boolean saveGame(World world){
+        return false;
+    }
+
+    public World loadGame(){
+        return null;
+    }
+}
