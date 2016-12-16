db729af8f3eaebd7e496ae58ba40039dfe9920f5
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-12-16 16:08:21 +0100

    Added Basic World and made Character abstract

--- TTOD2D/src/source/World.java
+++ TTOD2D/src/source/World.java
@@ -1,7 +1,30 @@
 package source;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+
 /**
  * Created by Michael on 16.12.2016.
  */
 public class World {
+    private ArrayList<Dungeon> dungeons;
+    private ArrayList<Town> towns;
+
+    public World (){
+        dungeons = new ArrayList<Dungeon>();
+        towns = new ArrayList<Town>();
+    }
+
+    public ArrayList<Dungeon> getDungeons(){
+        return dungeons;
+    }
+
+    public ArrayList<Town> getTowns() {
+        return towns;
+    }
+
+    public void generateDefault(){
+        throw new NotImplementedException();
+    }
 }
