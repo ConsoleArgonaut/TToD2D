b5d930be26accf5058bc8ef388c952e9cefa146f
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-12-17 13:11:31 +0100

    Added Types (Basic Enums)

--- TTOD2D/src/source/Types.java
+++ TTOD2D/src/source/Types.java
@@ -4,4 +4,27 @@ package source;
  * Created by Michael on 16.12.2016.
  */
 public class Types {
+    public enum dungeonType{
+
+    }
+
+    public enum buildingType{
+        Inn, Shop, Blacksmith, Unknown
+    }
+
+    public enum playerRace{
+
+    }
+
+    public enum combatActionResult{
+
+    }
+
+    public enum effect{
+
+    }
+
+    public enum enemyType{
+
+    }
 }
