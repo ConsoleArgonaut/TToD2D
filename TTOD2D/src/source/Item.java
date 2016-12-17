a8aa0490589037a3446f7abd4d7b5c76dd33d0cd
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-12-17 20:10:05 +0100

    Implemented Item

--- TTOD2D/src/source/Item.java
+++ TTOD2D/src/source/Item.java
@@ -1,7 +1,27 @@
 package source;
 
+
 /**
  * Created by Drake on 16.12.2016.
  */
 public class Item {
+    private String name;
+    private int maxCount;
+    private float value;
+    private boolean isCombatItem;
+
+    public Item(){
+        name = "";
+        maxCount = 0;
+        value = 0;
+        isCombatItem = false;
+    }
+    public String getName(){return name;}
+    public void setName(String nameToSet){name=nameToSet;}
+    public int getMaxCount(){return maxCount;}
+    public void setMaxCount(int maxCountToSet){maxCount = maxCountToSet;}
+    public float getValue(){return value;}
+    public void setValue(float valueToSet){value=valueToSet;}
+    public boolean getIsCombatItem(){return isCombatItem;}
+    public void setIsCombatItem(boolean isCombatItemToSet){isCombatItem=isCombatItemToSet;}
 }
