a2ed893e9e85db83e3d47e7b70fa33cc85364555
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-12-17 14:01:52 +0100

    Added Building and Inn

--- TTOD2D/src/source/Building.java
+++ TTOD2D/src/source/Building.java
@@ -4,5 +4,32 @@ package source;
  * Created by Michael on 16.12.2016.
  */
 public class Building {
+    private String name;
+    private Types.buildingType type;
 
+    public Building(){
+        name = "";
+        type = Types.buildingType.Unknown;
+    }
+
+    public Building(String buildingName, Types.buildingType buildingType){
+        name = buildingName;
+        type = buildingType;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String buildingName){
+        name = buildingName;
+    }
+
+    public Types.buildingType getType(){
+        return type;
+    }
+
+    public void setType(Types.buildingType buildingType){
+        type = buildingType;
+    }
 }
--- TTOD2D/src/source/Inn.java
+++ TTOD2D/src/source/Inn.java
@@ -1,7 +1,51 @@
 package source;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+
 /**
  * Created by Michael on 16.12.2016.
  */
 public class Inn extends Building {
+    private ArrayList<NPC> npcs;
+    private Innkeeper innkeeper;
+
+    public Inn(){
+        super("Inn", Types.buildingType.Inn);
+        npcs = new ArrayList<>();
+        innkeeper = new Innkeeper();
+    }
+
+    public Inn(Innkeeper innkeeperToSet, ArrayList<NPC> npcsToSet){
+        super("Inn", Types.buildingType.Inn);
+        innkeeper = innkeeperToSet;
+        npcs = npcsToSet;
+    }
+
+    public Inn(Innkeeper innkeeperToSet, ArrayList<NPC> npcsToSet, String innName){
+        super(innName, Types.buildingType.Inn);
+        innkeeper = innkeeperToSet;
+        npcs = npcsToSet;
+    }
+
+    public ArrayList<NPC> getNpcs(){
+        return npcs;
+    }
+
+    public void setNpcs(ArrayList<NPC> npcsToSet){
+        npcs = npcsToSet;
+    }
+
+    public Innkeeper getInnkeeper(){
+        return innkeeper;
+    }
+
+    public void setInnkeeper(Innkeeper innkeeperToSet){
+        innkeeper = innkeeperToSet;
+    }
+
+    public Status takeADump(){
+        throw new NotImplementedException();
+    }
 }
