84b6ba36b50e71e5d7892684a5bc7e6bb43fe8ae
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-12-18 09:59:08 +0100

    Implemented NPC and Shop

--- TTOD2D/src/source/NPC.java
+++ TTOD2D/src/source/NPC.java
@@ -1,7 +1,38 @@
 package source;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by Michael on 16.12.2016.
  */
 public class NPC extends Character {
+    private ArrayList<String> sentences;
+
+    public NPC(String npcName){
+        super();
+        sentences = new ArrayList<>();
+        setName(npcName);
+    }
+
+    public NPC(String npcName, ArrayList<String> npcSentences){
+        this(npcName);
+        sentences = npcSentences;
+    }
+
+    public ArrayList<String> getSentences(){
+        return sentences;
+    }
+
+    public void setSentences(ArrayList<String> sentencesToSet){
+        sentences=sentencesToSet;
+    }
+
+    public String talk(){
+        int min = 0;
+        int max = sentences.size() - 1;
+        Random random = new Random();
+        int randomNumber = random.nextInt((max - min) + 1) + min;
+        return sentences.get(randomNumber);
+    }
 }
--- TTOD2D/src/source/Shop.java
+++ TTOD2D/src/source/Shop.java
@@ -1,7 +1,37 @@
 package source;
 
+import java.util.ArrayList;
+
 /**
  * Created by Michael on 16.12.2016.
  */
 public class Shop extends Building {
+    private ArrayList<Trader> traders;
+
+    public Shop(){
+        this("Shop", Types.buildingType.Shop);
+    }
+
+    public Shop(Types.buildingType shopType){
+        this("Shop", shopType);
+    }
+
+    public Shop (String shopName){
+        this(shopName, Types.buildingType.Shop);
+    }
+
+    public Shop(String shopName, Types.buildingType shopType){
+        super(shopName, shopType);
+        traders = new ArrayList<>();
+    }
+
+    public Shop(String shopName, Types.buildingType shopType, ArrayList<Trader> tradersToSet){
+        this(shopName, shopType);
+        traders = tradersToSet;
+
+    }
+
+    public ArrayList<Trader> getTraders(){return traders;}
+
+    public void setTraders(ArrayList<Trader> tradersToSet){traders = tradersToSet;}
 }
