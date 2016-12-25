a190f7b2a9b96006cd98dbb399dc320637cbd1e6
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-12-25 16:29:10 +0100

    Implemented Floor

--- TTOD2D/src/source/Floor.java
+++ TTOD2D/src/source/Floor.java
@@ -1,7 +1,32 @@
 package source;
 
+import java.util.ArrayList;
+
 /**
  * Created by Michael on 16.12.2016.
  */
 public class Floor {
+    private ArrayList<Enemy> enemies;
+
+    public Floor(){
+        enemies = new ArrayList<>();
+    }
+
+    public Floor(ArrayList<Enemy> enemiesToSet){
+        enemies = enemiesToSet;
+    }
+
+    public ArrayList<Enemy> getEnemies(){
+        return enemies;
+    }
+
+    public void setEnemies(ArrayList<Enemy> enemiesToSet){
+        enemies = enemiesToSet;
+    }
+
+    public Combat getNextCombat(){
+        Enemy nextEnemy = enemies.get(0);
+        enemies.remove(nextEnemy);
+        return new Combat();
+    }
 }
