92c2d5bdcf8a7296f085738ebab5ee025cdaa3f1
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-12-27 13:31:02 +0100

    Implemented Enemy Controller Methods (with notimplementedexceptions)

--- TTOD2D/src/source/EnemyController.java
+++ TTOD2D/src/source/EnemyController.java
@@ -1,7 +1,22 @@
 package source;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+
 /**
  * Created by Drake on 16.12.2016.
  */
 public class EnemyController {
+    public ArrayList<Enemy> getEnemies(int count, ArrayList<Types.enemyType> possibleEnemies){
+        throw new NotImplementedException();
+    }
+
+    public Enemy getFloorBoss(Types.enemyType type, int currentFloor, int maxFloor){
+        throw new NotImplementedException();
+    }
+
+    public Enemy getDungeonBoss(Types.dungeonType type){
+        throw new NotImplementedException();
+    }
 }
