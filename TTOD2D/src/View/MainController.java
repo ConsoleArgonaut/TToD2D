cc819cbecc3e93af93757ee025550dbecfcd68e1
Paolo Stieger <paolo.stieger@gmail.com>
2017-01-16 21:28:22 +0100

    no message

---
+++ TTOD2D/src/View/MainController.java
@@ -0,0 +1,21 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController {
+    @FXML
+    private void handleOnBtnCreditsClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleOnBtnNewGameClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleOnBtnLoadGameClicked(ActionEvent event) {
+
+    }
+}
