1edf4b74e0dce76e10c65420ebf62a58dc4b9e87
Paolo Stieger <paolo.stieger@gmail.com>
2017-01-20 13:04:05 +0100

    View creation

--- TTOD2D/src/View/MainController.java
+++ TTOD2D/src/View/MainController.java
@@ -2,11 +2,32 @@ package View;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
-    private void handleOnBtnCreditsClicked(ActionEvent event) {
+    private Button btn1;
 
+    @FXML
+    private void handleOnBtnCreditsClicked(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+        if(event.getSource()==btn1){
+            //get reference to the button's stage
+            stage=(Stage) btn1.getScene().getWindow();
+            //load up OTHER FXML document
+            root = FXMLLoader.load(getClass().getResource("CreditsWindow.fxml"));
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     @FXML
