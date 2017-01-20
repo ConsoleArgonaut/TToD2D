905e126d746b6b6f248b05bd46eb8489da223fa5
Paolo Stieger <paolo.stieger@gmail.com>
2017-01-20 13:59:05 +0100

    Creation Testfolder and FileHandlerTest

---
+++ TTOD2D/src/JUnitTests/FileHandlerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * Created by pstieger on 20.01.2017.
+ */
+public class FileHandlerTest {
+    @Test
+    public void saveGame() throws Exception {
+
+    }
+
+    @Test
+    public void loadGame() throws Exception {
+
+    }
+
+}
