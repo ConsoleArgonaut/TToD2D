d1291b542549dd8a729a6e77c877da619de42f45
Kaiden-PC\Drake <kaiden3991@gmail.com>
2017-01-25 20:27:04 +0100

    added javadocs comments to class Armor

--- TTOD2D/src/source/Armor.java
+++ TTOD2D/src/source/Armor.java
@@ -7,12 +7,26 @@ package source;
  */
 
 public class Armor extends Item {
+
+    /**
+     * This variable is used to determine the defence value of the equipable
+     * @see Item of type
+     * @see Armor which is used to calculate damage in
+     * @see Combat
+     */
     private float defence = 0;
 
+    /**
+     * @return defence is returned when the method is called
+     */
     public float getDefence() {
         return defence;
     }
 
+    /**
+     * @param defence is used to assign and set the returned value as the defence value of
+     * @see Armor
+     */
     public void setDefence(float defence) {
         this.defence = defence;
     }
