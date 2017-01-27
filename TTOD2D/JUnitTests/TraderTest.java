51182e65e37a000585a106a3a7436e1f3ec56f89
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-01-27 15:13:28 +0100

    Added 1 Empty JUNIT Test

---
+++ TTOD2D/JUnitTests/TraderTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Michael on 27.01.2017.
+ */
+public class TraderTest {
+    @Test
+    public void getSellItemPrice() throws Exception {
+
+    }
+
+    @Test
+    public void getBuyItemPrice() throws Exception {
+
+    }
+
+    @Test
+    public void sellItem() throws Exception {
+
+    }
+
+    @Test
+    public void buyItem() throws Exception {
+
+    }
+
+}
