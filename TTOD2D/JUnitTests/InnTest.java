6076aadae14ac70570b8735acb5a5a01aac3c2d0
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-01-29 19:00:09 +0100

    Added Inn and Innkeeper tests

--- TTOD2D/JUnitTests/InnTest.java
+++ TTOD2D/JUnitTests/InnTest.java
@@ -1,4 +1,7 @@
+import org.junit.Assert;
 import org.junit.Test;
+import source.Inn;
+import source.Player;
 
 import static org.junit.Assert.*;
 
@@ -7,8 +10,11 @@ import static org.junit.Assert.*;
  */
 public class InnTest {
     @Test
-    public void takeADump() throws Exception {
-
+    public void takeADump() {
+        Player.getInstance().resetPlayer();
+        Inn inn = new Inn();
+        inn.takeADump();
+        Assert.assertTrue(Player.getInstance().getStatus() != null);
     }
 
 }
--- TTOD2D/JUnitTests/InnkeeperTest.java
+++ TTOD2D/JUnitTests/InnkeeperTest.java
@@ -1,4 +1,7 @@
+import org.junit.Assert;
 import org.junit.Test;
+import source.Innkeeper;
+import source.Player;
 
 import static org.junit.Assert.*;
 
@@ -7,8 +10,13 @@ import static org.junit.Assert.*;
  */
 public class InnkeeperTest {
     @Test
-    public void goSleeping() throws Exception {
-
+    public void goSleeping()  {
+        Player.getInstance().resetPlayer();
+        Innkeeper innkeeper = new Innkeeper("Test");
+        innkeeper.setRoomPrice(10);
+        Player.getInstance().setMoney(100);
+        Assert.assertTrue(innkeeper.goSleeping());
+        Assert.assertEquals(Player.getInstance().getMoney(), 90);
     }
 
 }
