a4adcd2b1d220835f37ebc3858f64ef27ce998f7
Kaiden-PC\Drake <kaiden3991@gmail.com>
2017-01-29 17:34:27 +0100

    Added JavaDocs comments to the Classes Types, Weapons and World

--- TTOD2D/src/source/Types.java
+++ TTOD2D/src/source/Types.java
@@ -7,30 +7,52 @@ package source;
  */
 
 public class Types {
+
+    /**
+     * This enum holds all dungeon types.
+     */
     public enum dungeonType{
         fire, water, earth, wind, ice, light, darkness, poison, lightning, unaspected, nature, metal, death
     }
 
+    /**
+     * This enum holds all building types.
+     */
     public enum buildingType{
         Inn, Shop, Blacksmith, Unknown
     }
 
+    /**
+     * This enum holds all player races.
+     */
     public enum playerRace{
         Unknown, Human, Dwarf, Dragonpeeps, Big_Daddys
     }
 
+    /**
+     * This enum holds all combat action results.
+     */
     public enum combatActionResult{
         ItemUsed, ItemMissed, Attacked, AttackMissed, Defended, Escaped, Waited, Evolved
     }
 
+    /**
+     * This enum holds all effect types.
+     */
     public enum effect{
         Good, Unrelenting, Stunned, Burning, Poisoned, Freezing, Constipated
     }
 
+    /**
+     * This enum holds all enemy types.
+     */
     public enum enemyType{
         Dragon, Humanoid, Goblinoid, Creature, Shepard, Undead, Lich, Steve, Pinguin, Treant, Dryad, Party_Pooper, Plant, Golem, Bird, Phantom, Titan
     }
 
+    /**
+     * This enum holds all item types.
+     */
     public enum itemType {
         Armor, Weapon, Potion, Focus, Poison, Shield,Scrap
     }
--- TTOD2D/src/source/Weapon.java
+++ TTOD2D/src/source/Weapon.java
@@ -7,21 +7,26 @@ package source;
  */
 
 public class Weapon extends Item {
+
+    /**
+     * This variable is used to save the attack value of the weapon.
+     */
     private float attack = 0;
+
+    /**
+     * Thsi variable is used to save the critical hit chance.
+     */
     private float critChance = 0;
 
     public float getAttack() {
         return attack;
     }
-
     public void setAttack(float attack) {
         this.attack = attack;
     }
-
     public float getCritChance() {
         return critChance;
     }
-
     public void setCritChance(float critChance) {
         this.critChance = critChance;
     }
