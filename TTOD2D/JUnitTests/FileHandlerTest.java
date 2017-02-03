68131a19e9a3f4cbc00d2ad385d00d2de7c699c0
Paolo Stieger <paolo.stieger@gmail.com>
2017-02-03 09:28:13 +0100

    CombatTestSuite Creation

---
+++ TTOD2D/JUnitTests/FileHandlerTest.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.junit.Test;
+import source.FileHandler;
+import source.Player;
+import source.World;
+
+import java.io.File;
+
+/**
+ * Created by pstieger on 03.02.2017.
+ */
+public class FileHandlerTest {
+    @Test
+    public void saveGame() throws Exception {
+        World world = new World();
+        world.generateDefault();
+        world.getDungeons().get(0).setCleared(true);
+        Player.getInstance().setName("S");
+        FileHandler fileHandler = new FileHandler();
+        fileHandler.saveGame(world);
+
+        Assert.assertTrue(doesFileExist("world.ser") && doesFileExist("player.ser"));
+    }
+
+    @Test
+    public void loadGame() throws Exception {
+        FileHandler fileHandler = new FileHandler();
+        World world = fileHandler.loadGame();
+
+
+        Assert.assertTrue(Player.getInstance().getName().equals("S") && world.getDungeons().get(0).isCleared());
+    }
+
+    private boolean doesFileExist(String fileName){
+        File file = new File(new java.io.File(".").getAbsolutePath() + fileName);
+
+        return file.exists();
+    }
+
+}
---
+++ TTOD2D/JUnitTests/TestRunner.java
@@ -0,0 +1,23 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+/**
+ * Created by pstieger on 03.02.2017.
+ */
+public class TestRunner {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(CombatTestSuite.class);
+
+        if(result.getFailureCount() > 0){
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
+        else {
+            System.out.println("Alle Tests erfolgreich abgeschlossen.");
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
+}
